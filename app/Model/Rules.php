4461f023b12d5e7c2ccb932188613d9a2099d8bb
clavius <savl.abax@gmail.com>
2020-05-22 21:46:02 +0300

    уточнение допустимых ставок

--- app/Model/Rules.php
+++ app/Model/Rules.php
@@ -40,6 +40,7 @@ class Rules
 
         if ($field <0 OR $field>36) Throw new \Exception ('Impossible field');
         if ($choose <0 OR $choose>36) Throw new \Exception ('Impossible choose');
+        if ($choose == 0 AND !in_array($type, ['black', 'red'])) Throw new \Exception ('Impossible choose');
 
         if ($field == self::ZERO) return 0;
 
