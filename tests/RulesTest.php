8687d4f8939b65131a97287654028701869b2604
clavius <savl.abax@gmail.com>
2020-05-23 14:43:16 +0300

    доп. тесты

--- tests/RulesTest.php
+++ tests/RulesTest.php
@@ -45,4 +45,68 @@ class RulesTest extends TestCase
         $this->assertEquals($check, 24);
     }
 
+    public function testBlack()
+    {
+        $rules = new Rules();
+
+        $check = $rules->checkField('black', 16, 6, 1);
+        $this->assertEquals($check, 0);
+
+        $check = $rules->checkField('black', 17, 7, 1);
+        $this->assertEquals($check, 2);
+
+    }
+
+    public function testRed()
+    {
+        $rules = new Rules();
+
+        $check = $rules->checkField('red', 16, 7, 1);
+        $this->assertEquals($check, 0);
+
+        $check = $rules->checkField('red', 17, 6, 1);
+        $this->assertEquals($check, 2);
+
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+
+    public function testWrongField()
+    {
+        $rules = new Rules();
+        $check = $rules->checkField('two', 16, -1, 1);
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+
+    public function testWrongChoose()
+    {
+        $rules = new Rules();
+        $check = $rules->checkField('two', 38, 2, 1);
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+
+    public function testWrongChooseZero()
+    {
+        $rules = new Rules();
+        $check = $rules->checkField('two', 0, 2, 1);
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+
+    public function testWrongType()
+    {
+        $rules = new Rules();
+        $check = $rules->checkField('error', 1, 2, 1);
+    }
+
 }
